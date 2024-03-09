ade1088e2bc1926d24c2d67d6bb3c0655a277ebb
King QuytU <quib2012247@student.ctu.edu.vn>
2024-03-09 23:48:51 +0700

    update

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -10,36 +10,36 @@ function Cards() {
         <div className="cards-wrapper">
           <ul className="cards-items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label = "Adventure"
-              path = "/services"
+              src="images/img-10.jpg"
+              text="Experience cloud hunting, sightseeing and taking photos in Da Lat"
+              label="Đà Lạt"
+              path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label = "Adventure"
-              path = "/services"
+              src="images/img-11.jpg"
+              text="Enjoy the wind and swim in Vung Tau beach"
+              label="Vũng Tàu"
+              path="/services"
             />
           </ul>
           <ul className="cards-items">
             <CardItem
-              src="images/img-1.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label = "Adventure"
-              path = "/services"
+              src="images/img-12.jpg"
+              text="Adventure on beautiful, poetic roads"
+              label="Hà Giang"
+              path="/services"
             />
             <CardItem
-              src="images/img-6.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label = "Adventure"
-              path = "/services"
+              src="images/img-13.jpg"
+              text="Resort, swimming and enjoying fresh seafood"
+              label="Phú Quốc"
+              path="/services"
             />
             <CardItem
-              src="images/img-7.jpg"
+              src="images/img-9.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label = "Adventure"
-              path = "/services"
+              label="Adventure"
+              path="/services"
             />
           </ul>
         </div>
