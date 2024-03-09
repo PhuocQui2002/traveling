3dfe7d8236adf264920a6dcfea811ca29a12b508
King QuytU <quib2012247@student.ctu.edu.vn>
2024-03-09 23:20:31 +0700

    update

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import Cards from "./Cards";
+import HeroSection from "../components/HeroSection";
+import Footer from "./Footer";
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+export default Home;
